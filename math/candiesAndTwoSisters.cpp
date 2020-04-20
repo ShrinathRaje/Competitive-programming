2ff2702ef302fcd6de50991b8e18fb1b9f1d94bd
Shrinath <developer.shrinath@gmail.com>
2020-04-20 23:18:11 +0530

    candies and two sisters | codeforces

---
+++ math/candiesAndTwoSisters.cpp
@@ -0,0 +1,20 @@
+//candies and two sisters
+//problem: http://codeforces.com/problemset/problem/1335/A
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--) {
+		long n;
+		cin >> n;
+
+		cout << (n-1)/2 << "\n";
+	}
+
+	return 0;
+}
